9eea6c028903a9605097e80651e052ad9bafec05
Hezaveh <mari.hezaveh@gmail.com>
2021-08-17 22:11:42 -0400

    setstate

--- 11reactBasics/class/src/App.js
+++ 11reactBasics/class/src/App.js
@@ -1,23 +1,56 @@
 import React from "react"
 
-// https://reactjs.org/docs/events.html#supported-events
-
-function handleClick() {
-    console.log("I was clicked")
-}
-
-function App() {
-    return (
-        <div>
-            <img onMouseOver={() => console.log("Hovered!")} src="https://www.fillmurray.com/200/100" />
-            <br />
-            <br />
-            <button onClick={handleClick}>Click me</button>
-        </div>
-    )
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+    
+    handleClick() {
+        this.setState(prevState => {
+            return {
+                count: prevState.count + 1
+            }
+        })
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.handleClick}>Change!</button>
+            </div>
+        )
+    }
 }
 
 export default App
+
+
+
+// import React from "react"
+
+// // https://reactjs.org/docs/events.html#supported-events
+
+// function handleClick() {
+//     console.log("I was clicked")
+// }
+
+// function App() {
+//     return (
+//         <div>
+//             <img onMouseOver={() => console.log("Hovered!")} src="https://www.fillmurray.com/200/100" />
+//             <br />
+//             <br />
+//             <button onClick={handleClick}>Click me</button>
+//         </div>
+//     )
+// }
+
+// export default App
 
 
 
